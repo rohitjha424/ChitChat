1cfc32e8ca5a86f17f515ef8f7e847cb008f3487
Rohit <rohitjha424@gmail.com>
2022-07-21 19:16:05 +0530

    link added into client

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io("https://ourchitchat.herokuapp.com/");
+const socket = io("https://ourchitchat.herokuapp.com");
 
 //Get DOM elements in respective Js variables
 const form = document.getElementById("send-container");
