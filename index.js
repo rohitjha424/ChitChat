d8147dcd9e86f09175e38f3d851def9eebf99044
Rohit <rohitjha424@gmail.com>
2022-07-21 18:31:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//node Server which will handle socket io connections
+// const io = require("socket.io")(8000);
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+//  var port = process.env.PORT || 3000
+
+
+const users = {};
+
+io.on("connection", (socket) => {
+    //If a new user joins the chat then others would get to know!
+  socket.on("new-user-joined", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+
+
+  //if someone sends a message, broadcast it to all people!
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {message: message, name: users[socket.id]});
+  });
+
+  //if someone leaves the chat, all others would get to know!
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+
+  });
+
+
+});
+
+http.listen(8000, function(){
+console.log('listening on *:8000');
+});
---
+++ js/client.js
@@ -0,0 +1,99 @@
+const socket = io("http://localhost:8000");
+
+//Get DOM elements in respective Js variables
+const form = document.getElementById("send-container");
+const messageInput = document.getElementById("messageInp");
+const messageContainer = document.querySelector(".container");
+
+//Audio that will play when someone recieves the message
+var audio = new Audio('ting.mp3');
+
+
+//Function to scroll the message container to latest message
+function scrollToLast(){
+    var myDiv = document.getElementById("myDiv");
+    myDiv.scrollTop = myDiv.scrollHeight; 
+}
+
+//Function which will append event info to the container
+const append = (message, position) => {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messageContainer.append(messageElement);
+  if(position=='left'){
+
+      audio.play();
+  }
+
+  scrollToLast();
+  
+};
+
+
+
+
+
+//Ask New user for his/her name by promt and let the server/all other people know
+const name = prompt("Enter your name to join");
+
+// checks if name is empty or null if it is then it again reloads the page and again asks!
+if(name == ""){
+    window.location.reload();
+  }
+if(name == null){
+   window.location.reload();
+  }
+ 
+socket.emit("new-user-joined", name);
+
+  //if a new user joins , Receive user's name from the server
+socket.on('user-joined', (name) => {
+  append(`${name} Joined the chat`, 'left');
+});
+
+//if server sends a message , receive it
+socket.on('receive', data => {
+    
+  append(`${data.name}: ${data.message}`, 'left');
+});
+
+//idf a user leaves the chat , append the info to the container/ let all know!
+socket.on('left', name => {
+  append(`${name} left the chat`, 'left');
+});
+
+
+//if the form get submitted/someone sends a message, send it to server
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    //checks if message is not null
+    if(message !==""){
+
+        append(`You: ${message}`, 'right');
+        socket.emit('send', message);
+        messageInput.value = '';
+        scrollToLast();
+    }
+})
+
+
+  
+
+
+// socket.emit("new-user-joined", name);
+
+// socket.on('user-joined', (name) => {
+//   append(`${name} Joined the chat`, 'left');
+// });
+
+
+// socket.on('receive', data => {
+//   append(`${data.name}: ${data.message}`, 'left');
+// });
+
+// socket.on('left', name => {
+//   append(`${name} left the chat`, 'left');
+// });
